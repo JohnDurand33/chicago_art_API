0df5c56b215301fe3bcd086ad930ead937257d0c
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-03-13 12:09:24 -0600

    updates

--- ChicagoArtInstituteAPI/main.js
+++ ChicagoArtInstituteAPI/main.js
@@ -4,12 +4,18 @@ function toggleLogin() {
     isLoggedIn = !isLoggedIn;
     const loginButton = document.getElementById('loginButton');
     const signupButton = document.getElementById('signup');
+    const h2Tag = document.querySelector('h2')
     
     if (isLoggedIn) {
         loginButton.textContent = 'Logout';
         signupButton.style.display = 'none';
+        h2Tag.style.paddingRight = '73px';
     } else {
         loginButton.textContent = 'Login';
         signupButton.style.display = 'inline';
+        h2Tag.style.paddingRight = '0px';
     }
-}
+    
+const clickedEvent = async () => {
+    
+};
